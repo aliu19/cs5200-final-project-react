86f7eb7542014d301523e5f0551701abbf4cd71e
aliu19 <liu.and@northeastern.edu>
2023-03-27 22:03:39 -0400

    update error page with ant

--- src/components/error/error.js
+++ src/components/error/error.js
@@ -1,16 +1,17 @@
 import { useRouteError } from "react-router-dom";
+import {Button, Result} from 'antd';
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-      <div id="error-page" className="position-absolute top-50 start-50 translate-middle">
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <i>{error.status}: {error.statusText || error.message}</i>
-        </p>
+      <div className="container-fluid position-absolute top-50 start-50 translate-middle">
+        <Result status={error.status}
+                title={error.status + ": " + (error.statusText || error.message)}
+                subTitle="Sorry, an unexpected error has occurred."
+                extra={<Button type="primary" href={"/"}>Back Home</Button>}
+        />
       </div>
   );
 }
